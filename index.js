04b9f15568241b49499b4d6e273b5615a892b152
Sudipto Das <dassudipto200@gmail.com>
2024-02-02 20:50:53 +0530

    adding private rooms feature

--- index.js
+++ index.js
@@ -7,27 +7,44 @@ const server = http.createServer(app);
 const io=new Server(server);
 
 //app.use(express.static(path.join(__dirname, "public")));
+const rooms = ['room1', 'room2', 'room3'];
 app.get('/', (req, res) => {
     
     res.sendFile(__dirname+"/public/index.html");
   });
-
-
-
   io.on('connection', (socket) => {
-    console.log(`a user connected`);
+    console.log(`a user connected ${socket.id}`);
+
+    io.to(socket.id).emit('updateRooms',rooms);
+    socket.on('joinRoom',(room)=>{
+      console.log(`${socket.id} joined ${room}`)
+      socket.join(room);
+    })
+    socket.on('privateMessage',(data)=>{
+      io.to(data.room).emit('privateMessage',{message:data.message,room:data.room,from:socket.id})
+    })
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
       });
   });
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log(msg)
-        io.emit('chat message', msg);
-    });
-  });
+
+  // io.on('connection',(socket)=>{
+  //   socket.on('joinRoom',(room)=>{
+  //     socket.join(room)
+  //     console.log(`${socket.id} joined ${room}`)
+  //   })
+  // })
+  // io.on('connection', (socket) => {
+    
+  //   socket.on('chat message', (msg) => {
+  //       console.log(msg)
+        
+  //       io.emit('chat message', msg);
+  //   });
+  // });
   
  
 server.listen(8000, () => {
-    console.log('listening on:3000');
+    console.log('listening on:8000');
   });
